6655bfb261561ff3d29199e8d4549406edfeade3
Andreas Kull <akullpp@gmail.com>
2020-01-14 09:29:18 +0100

    Add nvm and prezto

---
+++ binaries/nvm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
---
+++ binaries/prezto.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Ensure that you are in zsh"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+echo "Change default shell with chsh -s /bin/zsh"
