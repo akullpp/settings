aec7db5e7148ba09292af7c0ecf42c746dd940b8
Andreas Kull <akullpp@gmail.com>
2020-02-15 08:48:04 +0100

    Create eslint.node.js

---
+++ defaults/eslint.node.js
@@ -0,0 +1,15 @@
+"eslintConfig": {
+  "env": {
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "sourceType": "module"
+  },
+  "extends": [
+    "prettier"
+  ],
+  "plugins": [
+    "prettier"
+  ]
+}
