b64a68ad046f45a3be49c01a19f6734d4d2a1338
Andreas Kull <akullpp@gmail.com>
2020-02-19 21:32:25 +0100

    Create arch.sh

---
+++ binaries/arch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+sudo pacman -S \
+  bat \
+  docker \
+  exa \
+  gdm \
+  git \
+  glances \
+  httpie \
+  jdk-openjdk \
+  jq \
+  keepassxc \
+  p7zip \
+  powerline \
+  powerline-fonts \
+  reshift \
+  thefuck \
+  tldr \
+  zsh
+
+yay -S autojump \
+  google-chrome \
+  intellij-idea-ultimate-edition \
+  visual-studio-code-bin
+  
